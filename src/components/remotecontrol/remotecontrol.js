80ea15a3fb65feb3e3bc97a2d7a0fd19e1bf8493
Damian Kacperski <7dami77@gmail.com>
2024-01-11 01:38:33 +0100

    Move events from buttonMute that should be watched by the parent

--- src/components/remotecontrol/remotecontrol.js
+++ src/components/remotecontrol/remotecontrol.js
@@ -546,6 +546,7 @@ export default function () {
         console.debug('remotecontrol event: ' + e.type);
         const player = this;
 
+        buttonMute.onPlaybackStopped(player, e, state);
         if (!state.NextMediaType) {
             updatePlayerState(player, dlg, {});
             appRouter.back();
@@ -560,6 +561,7 @@ export default function () {
         const player = this;
         updatePlayerState(player, dlg, state);
         onPlaylistUpdate();
+        buttonMute.onStateChanged(player, event, state);
     }
 
     function onTimeUpdate() {
@@ -576,6 +578,7 @@ export default function () {
     function onVolumeChanged() {
         const player = this;
         updatePlayerVolumeState(dlg, player.isMuted(), player.getVolume());
+        buttonMute.onVolumeChanged(player);
     }
 
     function releaseCurrentPlayer() {
@@ -814,7 +817,9 @@ export default function () {
     }
 
     function onPlayerChange() {
-        bindToPlayer(dlg, playbackManager.getCurrentPlayer());
+        const player = playbackManager.getCurrentPlayer();
+        bindToPlayer(dlg, player);
+        buttonMute.onPlayerChange(dlg, player);
     }
 
     function onMessageSubmit(e) {
@@ -911,12 +916,12 @@ export default function () {
     };
 
     self.onShow = function () {
-        onShow(dlg);
         buttonMute.onShow();
+        onShow(dlg);
     };
 
     self.destroy = function () {
-        onDialogClosed();
         buttonMute.destroy();
+        onDialogClosed();
     };
 }
